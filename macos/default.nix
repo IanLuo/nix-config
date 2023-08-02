871ee155abcf4bc1187c8089333f1077876ac3ed
ian luo <ianluo63@gmail.com>
2023-08-02 19:52:53 +0800

    update

--- macos/default.nix
+++ macos/default.nix
@@ -1,81 +1,66 @@
 { pkgs, lib, ... }:
-let brewBinPrefix = if pkgs.system == "aarch64-darwin" then "/opt/homebrew/bin" else "/usr/local/bin";
-in
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.ianluo = { pkgs, ... }: with lib; {
-    home.stateVersion = "23.05";
+  home-manager.users.ianluo = { pkgs, ... }: with lib;
+    {
+      home.stateVersion = "23.05";
 
-    home.packages = with pkgs; [
-      git
-      direnv
-      curl
-      wget
-      zsh
-      tree
-      any-nix-shell
-      tmate
-      tmux
-      nodejs
-      dbeaver
-      iterm2
-      devbox
-      element-desktop
-      spacevim
-    ] ++ lib.optionals stdenv.isDarwin [
-      m-cli
-    ];
+      home.packages = with pkgs; [
+        git
+        direnv
+        curl
+        wget
+        zsh
+        tree
+        any-nix-shell
+        tmate
+        tmux
+        nodejs
+        dbeaver
+        iterm2
+        devbox
+        element-desktop
+        spacevim
+        fd
+        raycast
+        ripgrep
+      ] ++ lib.optionals stdenv.isDarwin [
+        m-cli
+      ];
 
-    imports = [
-      # ../programs/vim/vim.nix
-      # ../programs/alacritty/alacritty.nix
-      ../programs/tmux/tmux.nix
-      ../programs/gitui/gitui.nix
-      (import ../programs/zsh/zsh.nix { inherit pkgs lib brewBinPrefix; })
-    ];
+      imports = [
+        # ../programs/vim/vim.nix
+        # ../programs/alacritty/alacritty.nix
+        ../programs/tmux/tmux.nix
+        ../programs/gitui/gitui.nix
+        (import ../programs/zsh/zsh.nix { inherit pkgs lib brewBinPrefix; })
+      ];
 
+      programs.command-not-found.enable = true;
 
-    programs.command-not-found.enable = true;
-
-    programs.htop = {
-      enable = true;
-      settings.show_program_path = true;
-    };
-
-    programs.fzf = {
-      enable = true;
-    };
-
-    programs.git = {
-      enable = true;
-      extraConfig = {
-        core.editor = "vi";
+      programs.htop = {
+        enable = true;
+        settings.show_program_path = true;
       };
-    };
 
-    programs.tmate.enable = true;
-  };
+      programs.fzf = {
+        enable = true;
+      };
+
+      programs.git = {
+        enable = true;
+        extraConfig = {
+          core.editor = "vi";
+        };
+      };
+
+      programs.tmate.enable = true;
+    };
 
 
   # make sure the nix daemon is always runs
   services.nix-daemon.enable = true;
   # install a version of nix, that doesn't need 'experimental-features = nix-command flakes" in /etc/nix/nix.conf 
   # services.nix-daemon.package = pkgs.nixFlakes;
-
-
-  homebrew = {
-    enable = true;
-    onActivation.autoUpdate = false;
-    # updates homebrew packages on activation
-    # can make darwin-rebuid much slower but otherwise will do it manually
-    brewPrefix = brewBinPrefix;
-    casks = [
-      "raycast"
-    ];
-
-    brews = [
-      "xcodegen"
-    ];
-  };
 } 
