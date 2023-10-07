86029c19d54c98dcaad1a660e03476531459b2e8
ian luo <ianluo63@gmail.com>
2023-10-07 23:48:21 +0800

    update indent

--- programs/vim/config/lua/plugins/indent-blankline-config.lua
+++ programs/vim/config/lua/plugins/indent-blankline-config.lua
@@ -21,10 +21,4 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-require("ibl").setup {
-    indent = { highlight = highlight },
-    whitespace = {
-        highlight = highlight,
-    },
-    scope = { enabled = false },
-}
+require("ibl").setup { indent = { highlight = highlight } }
