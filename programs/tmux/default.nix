b9207a7a855b0bef9de22b04df41d4166f12baca
ian luo <ianluo63@gmail.com>
2023-10-13 23:23:02 +0800

    update tmux defaut new window path

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -25,7 +25,7 @@
       # easy-to-remember split pane commands
         bind | split-window -h -c "#{pane_current_path}"
         bind - split-window -v -c "#{pane_current_path}"
-        bind c new-window -c "#{pane_current_path}"
+        bind c new-window -c "~"
 
         unbind C-b
         set -g prefix C-a
