9af551ca635c894ab9413cad0d5330ab1e16c669
Ian Luo <iluo@aligntech.com>
2024-02-21 13:19:07 +0800

    add some key binding for skhd

--- services/skhd/default.nix
+++ services/skhd/default.nix
@@ -15,6 +15,13 @@
       # float / unfloat window and center on screen
       alt - t : yabai -m window --toggle float;\
                 yabai -m window --grid 4:4:1:1:2:2
+
+      # toggle window fullscreen zoom
+      alt - f : yabai -m window --focus mouse && \
+                yabai -m window --toggle zoom-fullscreen
+
+      # balance size of windows
+      alt - 0 : yabai -m space --balance
     '';
     };
 }
