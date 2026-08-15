d23f078e10691070a671b0ba0b2b488ff081c4d0
ian luo <ianluo63@gmail.com>
2026-08-15 11:59:04 +0800

    hm: restore packages/custom/default.nix (empty custom package set)

---
+++ packages/custom/default.nix
@@ -0,0 +1,5 @@
+{ pkgs, unstable-pkgs ? pkgs, definitions ? { } }:
+# No custom packages currently defined.
+# Re-add derivations here (e.g. pkgs.callPackage ./foo.nix { }) to include
+# them in home.packages via packages/default.nix.
+{ }
