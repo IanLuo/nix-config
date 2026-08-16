8bd9fbb180d931c882023eecce41faf4a7cff8eb
ian luo <ianluo63@gmail.com>
2026-08-16 13:03:22 +0800

    hm: bind Neogit to <leader>gg

--- programs/vim/config/lua/core/keymaps.lua
+++ programs/vim/config/lua/core/keymaps.lua
@@ -31,6 +31,7 @@ keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git 
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
 keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list available help tags
+keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>") -- open Neogit (full git UI)
 
 -- restart lsp server (not on youtube nvim video)
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
